cb1ae86e77dae3b230bc16527951828c10a1ec83
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-06 17:38:15 +0530

    Create b-3.py

---
+++ b-3.py
@@ -0,0 +1,9 @@
+print("to check whether the input is vowel or not")
+x=input("enter your value")
+v=["a","e","i","o","u"]
+for y in v:
+      if(y==x):
+      print("vowel")
+      break
+ else:
+      print("the input is invalid")
