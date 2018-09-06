5b7bf1eae553c29c22c14ddb3020fb879c99dae6
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-06 17:48:04 +0530

    Create b-4.py

---
+++ b-4.py
@@ -0,0 +1,9 @@
+print("to check whether the input is Alphabet or not")
+x="a"
+v=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
+for y in v:
+    if(y==x):
+      print("Alphabet")
+      break
+    else:
+       print("no") 
