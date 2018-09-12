0d1b26517395941630a7384f0421b1cf6265af4b
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-12 12:28:37 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+print("DISPLAY PRIME NUMBERS BETWEEN 2 INTERVALS")
+  s=int(input("s="))
+  e=int(input("e="))
+  flag=0
+ for x in range(s+1,e):
+  flag=0
+ for y in range(1,x+1):
+  if(x%y==0):
+    flag=flag+1
+   if(flag==2)
+    print(x)
