c8807c050d7e1d77a10aa1d14986c5de6ea53e36
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-24 17:55:47 +0530

    Create permutation.py

---
+++ permutation.py
@@ -0,0 +1,8 @@
+def all permutations(str):
+    permList=permutations(str)
+   for perm in list(permList) :
+      print(".join(perm))
+      if-name-=="-main-":
+        str='123'
+        all permutations (str)
+    
