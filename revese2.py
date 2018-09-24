2f8770305d838bf16d9c9609760b5a4ded467d06
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-24 17:29:43 +0530

    Create revese2.py

---
+++ revese2.py
@@ -0,0 +1,11 @@
+def reverse(s):
+    if len==0:
+          return s
+        else:
+          return reverse(s[1:])+s[0]
+       s="hello world"   
+        print("the original string is :"end="")
+          print(s)
+         print("the reversed string is:",end="") 
+            print(reverse(s)
+          
