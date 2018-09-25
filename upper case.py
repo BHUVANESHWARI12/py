df47dd83f37ecd7864461bbde54834839a402a3a
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-25 16:56:51 +0530

    Create upper case.py

---
+++ upper case.py
@@ -0,0 +1,12 @@
+string=raw-input("enter string:")
+  count1=0
+  count2=0
+    for x in string:
+      if(i.islower()):
+          count 1=count 1+1
+       elif(i.isupper()):
+          count 2=count 2+1
+        print("the number of lower case character is:")  
+            print(count 1)
+        print("the number of upper case charecter is:") 
+            print(count 2)
