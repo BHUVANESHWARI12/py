44b8f2a7b246255db1e9f1f6ba534ccf14fc32b4
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-26 15:31:54 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,10 @@
+n=(input("N="))
+  if(len(n)<=20):
+      i=len(n)-1
+      rev=""
+      s=0
+    while(i>=0)  
+      1={"I":1,"II":2,"III":3,"IV":4,"V":5,"VI":6,"VII":7,"VIII":8,"IX":9,"X":10}
+      S=S+1[rev]
+      i=i-1
+     print(s) 
