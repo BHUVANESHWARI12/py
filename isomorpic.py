b1b8121d515731673003943fa65327c695cb8740
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-09-26 16:30:42 +0530

    Create isomorpic.py

---
+++ isomorpic.py
@@ -0,0 +1,12 @@
+int main()
+{
+    string string_1;
+    string string_2;
+        cin>>string_1;
+        cin>>string_2;
+            if(areisomorphic(string_1,string_2))
+              cout<<"true";
+             else:
+              cout<<"false";
+                return 0
+    
