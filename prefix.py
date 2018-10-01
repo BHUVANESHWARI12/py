9372fe7fa70a753b3c5e5e96bda3b9f8a6e8d647
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-10-01 14:49:17 +0530

    Create prefix.py

---
+++ prefix.py
@@ -0,0 +1,10 @@
+public string longestCommonPrefix(string[]str){
+    if(strs.length==0)return"");
+      string prefix=strs[0];
+      for(int i=1;i<strs.length;i++)
+      while(strs[i].index of (prefix)!=0){
+          prefix=prefix.substring(0,prefix.length()-1);
+              if(prefix.isempty())return"";
+              }
+              return prefix;
+              }
