7ffdc0478672f554083f6c07eba60d95868c33df
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2018-10-01 14:32:39 +0530

    Create smallest num.py

---
+++ smallest num.py
@@ -0,0 +1,10 @@
+n=int(input("enter the number:"))
+m=int(input("enter the digit to delete:"))
+  if(1<=n<=1000000000000000000):
+      a=str(n)[::-1]
+      b=int(a)//(10**m)
+        print(str(b)[::-1])
+        else:
+            print("limit is exist")
+      
+    
