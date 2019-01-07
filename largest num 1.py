326fe916f59d97507e4dd7f52244ad2758fc7257
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2019-01-07 15:04:41 +0530

    Rename largest num.py to largest num 1.py

---
+++ largest num 1.py
@@ -0,0 +1,10 @@
+def findMaxNum(arr,n):
+  arr.sort(reverse=true)
+  num=arr[0]
+    for i in range(1,n):
+      num=num*10+arr[i]
+      return num
+      if_name_=="_main_"
+      arr[5,6,7,8,9]
+      n=len(arr)
+      print(findMaxNum(arr,n))
