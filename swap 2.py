f7cbf33f809fb6a71b410d1074748c78d84780b7
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2019-01-09 14:52:14 +0530

    Create swap 2.py

---
+++ swap 2.py
@@ -0,0 +1,12 @@
+n1=input("Enter 1st number:");
+if n1=='a';
+    exit();
+    else:
+      n2=input("Enter 2nd number:");
+      num 1=int(n1);
+      num 2=int(n2);
+      print("After swapping")
+        swap=num1;
+        num 1=num2;
+        num 2=swap2;
+        print("1st number=",num1)
