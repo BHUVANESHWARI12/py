f3824134c6a8b1b969c167209022ef34c5ac80cd
BHUVANESHWARI12 <41039943+BHUVANESHWARI12@users.noreply.github.com>
2019-01-09 15:32:29 +0530

    Create sub 1.py

---
+++ sub 1.py
@@ -0,0 +1,12 @@
+a1=int(input("hour:"))
+b1=int(input("minute:"))
+a2=int(input("enter the second hourand minute;hour:"))
+b2=int(input("minute:"))
+a=a1-a2
+b=b1-b2
+if(b<0):
+ae=a-1
+be=60+b
+print(ae,":",be)
+else:
+print(a,":",b)
